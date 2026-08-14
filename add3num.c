1ba625091c28e9ca00e260e44fb5551d190e784c
patilshreya46075-art <patilshreya46075@gmail.com>
2026-08-14 06:00:14 +0000

    addition of 3num

---
+++ add3num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int n,m,o;
+printf("enter 1st num:");
+scanf("%d",&n);
+printf("enter 2nd num:");
+scanf("%d",&m);
+printf("enter 3rd num:");
+scanf("%d",&o);
+printf("the addition is:%d",n+m+o);}
