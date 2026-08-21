f6a9e3a9dcd76c319a71f18a69a1c0b9f647e10d
patilshreya46075-art <patilshreya46075@gmail.com>
2026-08-21 12:06:59 +0530

    Create swapnum.cpp

---
+++ swapnum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, temp;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After swapping: a = " << a << ", b = " << b;
+    return 0;
+}
